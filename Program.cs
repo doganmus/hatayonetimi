d70cb230e9b1c5bb5df201d9906885063b94ed3e
Dogan Mus <doganmus87@gmail.com>
2022-03-31 22:52:20 +0300

    ders sonu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,56 @@ namespace hata_yonetimi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           /* try
+            {
+            Console.WriteLine("Bir Sayı Giriniz");
+            int sayi =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı:"+sayi); 
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata :"+ex.Message.ToString());
+            }
+            finally
+            {
+                Console.Write("İşlem Tamamlandı");
+            } */
+
+            try
+            {
+               //int a = int.Parse(null); 
+               //int a = int.Parse("test");
+               int a = int.Parse("-200000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+
+
+            } 
+            catch (FormatException ex)
+            {
+                
+                Console.WriteLine("Veri tipi uygun değil");
+                Console.WriteLine(ex);
+
+
+            } 
+            catch (OverflowException ex)
+            {
+                
+                Console.WriteLine("Çok küçük veya çok büyük bir değer girdiniz");
+                Console.WriteLine(ex);
+
+
+            } 
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı");
+            }
+            
         }
     }
 }
